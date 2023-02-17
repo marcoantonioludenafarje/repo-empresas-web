4bde567a064f3853b63d2a7e83e8f812b7fc361d
Marcial(Acaedric) <acaedric@gmail.com>
2023-02-17 04:53:22 -0500

    descargar routeTemplateGenerator

--- src/modules/sample/Distribution/NewRoute.js
+++ src/modules/sample/Distribution/NewRoute.js
@@ -726,8 +726,24 @@ const Distribution = (props) => {
 
   useEffect(() => {
     if (excelTemplateGeneratedToRouteRes) {
+      const byteCharacters = atob(excelTemplateGeneratedToRouteRes);
+      const byteNumbers = new Array(byteCharacters.length);
+      for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+      }
+      const byteArray = new Uint8Array(byteNumbers);
+      const blob = new Blob([byteArray], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'createRouteTemplate.xlsx');
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
     }
   }, [excelTemplateGeneratedToRouteRes]);
+  
+  
 
   const handleExport = () => {
     exportToExcel(products, deliveries);
