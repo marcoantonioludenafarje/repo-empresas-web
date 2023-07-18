9503a19b097d115f926b4394d97f296b437654c9
Emerson Villalta <reaperfocs@gmail.com>
2023-07-18 05:06:34 -0500

    Add update, del(not function), bottomnNEWe, ilters not working

--- src/modules/sample/Crm/Views.js
+++ src/modules/sample/Crm/Views.js
@@ -7,9 +7,28 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
-import {Button} from '@mui/material';
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  CircularProgress,
+  Menu,
+  TextField,
+  Select,
+  ButtonGroup,
+  Stack,
+  useTheme,
+  useMediaQuery,
+} from '@mui/material';
+import {makeStyles} from '@mui/styles';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-
+import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
+import ManageSearchOutlinedIcon from '@mui/icons-material/ManageSearchOutlined';
+import CachedIcon from '@mui/icons-material/Cached';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import GridOnOutlinedIcon from '@mui/icons-material/GridOnOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import {getCampaigns} from '../../../redux/actions/Campaign';
 import {convertToDate} from '../../../Utils/utils';
 import {useDispatch, useSelector} from 'react-redux';
@@ -19,28 +38,31 @@ import {
   GET_USER_DATA,
   CREATE_CAMPAIGN,
 } from '../../../shared/constants/ActionTypes';
+import Router from 'next/router';
 
 function createData(name, fecha, containt, receipt) {
   return {name, fecha, containt, receipt};
 }
 
-const rows = [
-  createData(
-    'Nueva Dental',
-    '12-07-2023',
-    'Por apertura descuentos en curaciones',
-    'TODOS LOS CLIENTES',
-  ),
-  createData(
-    'Mejores Defensas bucales',
-    '12-07-2023',
-    'Recreación de dientes, curaciones',
-    'ALGUNOS CLIENTES',
-  ),
-];
+const useStyles = makeStyles((theme) => ({
+  btnGroup: {
+    marginTop: '2rem',
+  },
+  btnSplit: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  stack: {
+    justifyContent: 'center',
+    marginBottom: '10px',
+  },
+}));
 
-export default function Views() {
+export default function Views(props) {
+  const classes = useStyles(props);
   const dispatch = useDispatch();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   const getCampaign = (payload) => {
     dispatch(getCampaigns(payload));
@@ -79,8 +101,72 @@ export default function Views() {
     }
   }, [userDataRes]);
 
+  let selectedClient = {};
+  let codProdSelected = '';
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  /* let anchorEl = null; */
+  const openMenu = Boolean(anchorEl);
+  const handleClick = (codPro, event) => {
+    console.log('evento', event);
+    console.log('index del map', codPro);
+    setAnchorEl(event.currentTarget);
+    codProdSelected = codPro;
+    selectedClient =
+      listCampaigns[codPro]; /* .find((obj) => obj.client == codPro); */
+    console.log('Select Campaña', selectedClient);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const goToUpdate = () => {
+    console.log('Actualizando', selectedClient);
+    Router.push({
+      pathname: '/sample/clients/update',
+      query: selectedClient,
+    });
+  };
+
+  const setDeleteState = () => {
+    setOpen2(true);
+    handleClose();
+  };
+
+  let popUp = false;
+
+  const newClient = () => {
+    console.log('Para redireccionar a nuevo cliente');
+    Router.push('/sample/crm/create');
+  };
+
   return (
     <Card sx={{p: 4}}>
+      <Stack
+        sx={{m: 2}}
+        direction={isMobile ? 'column' : 'row'}
+        spacing={2}
+        className={classes.stack}
+      >
+        <TextField
+          label='Nombre de campaña'
+          variant='outlined'
+          name='nameToSearch'
+          size='small'
+          onChange={''}
+        />
+        <Button startIcon={<FilterAltOutlinedIcon />} variant='outlined'>
+          Más filtros
+        </Button>
+        <Button
+          startIcon={<ManageSearchOutlinedIcon />}
+          variant='contained'
+          color='primary'
+          onClick={''}
+        >
+          Buscar
+        </Button>
+      </Stack>
       <span>{`Items: ${
         listCampaigns ? listCampaigns.length : 'Cargando...'
       }`}</span>
@@ -102,9 +188,9 @@ export default function Views() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {listCampaigns?.map((row) => (
+            {listCampaigns?.map((row, index) => (
               <TableRow
-                key={row.campaignId}
+                key={index}
                 sx={{'&:last-child td, &:last-child th': {border: 0}}}
               >
                 <TableCell>{convertToDate(row.createdAt)}</TableCell>
@@ -118,7 +204,13 @@ export default function Views() {
                 </TableCell>
                 <TableCell>{row.receivers.length}</TableCell>
                 <TableCell>
-                  <Button id='basic-button'>
+                  <Button
+                    id='basic-button'
+                    aria-controls={openMenu ? 'basic-menu' : undefined}
+                    aria-haspopup='true'
+                    aria-expanded={openMenu ? 'true' : undefined}
+                    onClick={handleClick.bind(this, index)}
+                  >
                     <KeyboardArrowDownIcon />
                   </Button>
                 </TableCell>
@@ -127,6 +219,63 @@ export default function Views() {
           </TableBody>
         </Table>
       </TableContainer>
+      <ButtonGroup
+        variant='outlined'
+        aria-label='outlined button group'
+        className={classes.btnGroup}
+      >
+        {localStorage
+          .getItem('pathsBack')
+          .includes('/inventory/campaigns/register') === true ? (
+          <Button
+            variant='outlined'
+            startIcon={<AddCircleOutlineOutlinedIcon />}
+            onClick={newClient}
+          >
+            Nuevo
+          </Button>
+        ) : null}
+
+        {localStorage
+          .getItem('pathsBack')
+          .includes('/inventory/exportClients/*') === true ? (
+          <Button
+            variant='outlined'
+            startIcon={<GridOnOutlinedIcon />}
+            onClick={''}
+          >
+            Exportar todo
+          </Button>
+        ) : null}
+
+        {!popUp ? <></> : <CircularProgress disableShrink sx={{m: '10px'}} />}
+      </ButtonGroup>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={openMenu}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        {localStorage
+          .getItem('pathsBack')
+          .includes('/inventory/clients/update') === true ? (
+          <MenuItem onClick={goToUpdate}>
+            <CachedIcon sx={{mr: 1, my: 'auto'}} />
+            Actualizar
+          </MenuItem>
+        ) : null}
+        {localStorage
+          .getItem('pathsBack')
+          .includes('/inventory/clients/delete') === true ? (
+          <MenuItem onClick={setDeleteState}>
+            <DeleteOutlineOutlinedIcon sx={{mr: 1, my: 'auto'}} />
+            Eliminar
+          </MenuItem>
+        ) : null}
+      </Menu>
     </Card>
   );
 }
